aaa045091d9d786315fa18ec30955043dc13e9b9
Amkeez252 <auwalamalik@gmail.com>
2023-03-25 15:18:08 -0700

    task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# puppet declarative script to grab puppet-lint
-package { 'flask':
-  ensure   => '21.0',
-  provider => 'gem',
+#!/usr/bin/pup
+# Install a specific version of flask (2.1.0)
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
