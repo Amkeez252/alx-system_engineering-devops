5b96859641ea3730d213aa6217c96b1f3f0ea2b8
Amkeez252 <auwalamalik@gmail.com>
2023-05-09 19:51:22 -0700

    fix errors

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,26 +4,16 @@
 function that queries the Reddit API and 
 Returns the number of subscribers
 """
-from json import loads
-from requests import get
+import requests
 
 
 def number_of_subscribers(subreddit):
-    """ecursive function that queries the Reddit API and returns a list
-    containing the titles of all hot articles for a given subreddit. If no
-    results are found for the given subreddit, the function should return None
-    """
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {
-        'User-Agent':
-        'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
-        Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
-    }
-    response = get(url, headers=headers)
-    reddits = response.json()
-
-    try:
-        subscribers = reddits.get('data').get('subscribers')
-        return int(subscribers)
-    except:
+    """ Return number of subscribers """
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    Apiurl = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    resp = requests.get(Apiurl, headers=headers, allow_redirects=False)
+    if resp.status_code == 200:
+        data = resp.json()
+        return data['data']['subscribers']
+    else:
         return 0
