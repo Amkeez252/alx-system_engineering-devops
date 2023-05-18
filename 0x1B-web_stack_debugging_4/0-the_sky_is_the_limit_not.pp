cef6fc3565db8f4f3e6f1eeda76c74423652d2b0
Amkeez252 <auwalamalik@gmail.com>
2023-05-18 15:07:37 -0700

    web_stack_debugging

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,15 @@
+# Increase limit
+
+exec { 'Increase limit':
+  command => 'sed -i "s/15/100000/" /etc/default/nginx',
+  path    => ['/usr/bin', '/bin',],
+  returns => [0,1]
+}
+
+service { 'nginx':
+  ensure     => running,
+  enable     => true,
+  hasrestart => true,
+  restart    => '/usr/sbin/service nginx restart',
+  subscribe  => Exec['Increase limit'],
+}
